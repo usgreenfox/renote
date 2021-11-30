8542c1507dce8f58f0f1758fa592e09fae16c63d
usgreenfox <usgreenfox@gmail.com>
2021-11-30 00:10:47 +0000

    Add routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :notes do
+    resources :comments, only:[:create, :destroy]
+    resource :bookmarks, only:[:create, :destroy]
+    resource :reminds, only:[:create, :update, :destroy]
+  end
+  resources :users, only:[:show, :edit, :update]
+  get 'search' => 'seeaches#index', as:'search'
+
 end
