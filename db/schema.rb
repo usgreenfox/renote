4c5f15f047e4b80dd13b0a896287dac0090185c4
usgreenfox <usgreenfox@gmail.com>
2021-11-30 06:03:44 +0000

    Create comments_controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_30_041615) do
+ActiveRecord::Schema.define(version: 2021_11_30_054600) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "note_id"
+    t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["note_id"], name: "index_comments_on_note_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "notes", force: :cascade do |t|
     t.integer "user_id"
