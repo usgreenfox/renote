d15003d988f249a07a7f3d399b52dac57ed76bb2
usgreenfox <usgreenfox@gmail.com>
2021-12-02 05:28:45 +0000

    Create views for devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
 
   protected
   def configure_permitted_parameters
-    devise_patameter_sanitizer.permit(:sign_up,keys:[:name])
+    devise_parameter_sanitizer.permit(:sign_up,keys:[:name])
   end
 end
