40c63eefc5c2cf5c2d40142d1afeab09c34f21c4
usgreenfox <usgreenfox@gmail.com>
2021-12-06 04:47:59 +0000

    Fix a typo

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -9,8 +9,8 @@ class SearchesController < ApplicationController
   def search_for(model, keyword)
     # タグのバッジをクリック時、完全一致
     if model == 'tag'
-      Tag.find_by(name: keyword).notes
-    # 検索窓に入力時、
+      Tag.find_by("name =?", keyword).notes
+    # 検索窓に入力時、部分一致
     elsif model == 'note'
       Note.eager_load(:tags).where("title LIKE? or body LIKE? or tags.name LIKE?", "%#{keyword}%", "%#{keyword}%", "%#{keyword}%")
     end
