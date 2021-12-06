3e91453e3a149ae195e1cf750f5bb239c847e757
usgreenfox <usgreenfox@gmail.com>
2021-12-06 04:35:45 +0000

    Add search with tag

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,15 +2,17 @@ class SearchesController < ApplicationController
   def index
     model = params[:model]
     keyword = params[:keyword]
-    
+
     @results = search_for(model, keyword)
   end
-  
+
   def search_for(model, keyword)
+    # タグのバッジをクリック時、完全一致
     if model == 'tag'
-      
+      Tag.find_by(name: keyword).notes
+    # 検索窓に入力時、
     elsif model == 'note'
-      Note.where("title LIKE? or body LIKE?", "%#{keyword}%", "%#{keyword}%")
+      Note.eager_load(:tags).where("title LIKE? or body LIKE? or tags.name LIKE?", "%#{keyword}%", "%#{keyword}%", "%#{keyword}%")
     end
   end
 end
