2966fb45f999dc6364e742873126d23ba5c7ab40
usgreenfox <usgreenfox@gmail.com>
2021-12-09 07:20:38 +0000

    Fix typos

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,5 +1,5 @@
 class BookmarksController < ApplicationController
-  before_action :set_note, only: %i(create, destroy)
+  before_action :set_note, only: %i(create destroy)
 
   def create
     bookmark = current_user.bookmarks.find_or_create_by(note_id: @note.id)
