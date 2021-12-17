334cd40e6ea4dd6d9535ab370f9278b48f55e063
usgreenfox <usgreenfox@gmail.com>
2021-12-17 04:04:40 +0000

    Fix notes validation

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -30,13 +30,22 @@ class NotesController < ApplicationController
     @note = current_user.notes.new(note_params)
     tag_list = params[:note][:tag_name].split(nil)
     if @note.save
-      #タグの登録
+      # sessionの初期化
+      session[:note] = nil
+
+      # タグの登録
       @note.save_tag(tag_list)
-      #リマインドの作成
+
+      # リマインドの作成
       current_user.reminds.find_or_create_by(note_id: @note.id)
+
       redirect_to note_path(@note), notice: "ノートが作成されました"
     else
-      render :new
+      # フォームから渡された値のみsessionに保存（user_id等は保存しない）
+      session[:note] = @note.attributes.slice(*note_params.keys)
+
+      flash[:alert] = @note.errors.full_messages
+      redirect_to new_note_path
     end
   end
 
@@ -46,7 +55,8 @@ class NotesController < ApplicationController
       @note.save_tag(tag_list)
       redirect_to note_path(@note), notice: "ノートが更新されました"
     else
-      render 'edit'
+      flash[:alert] = @note.errors.full_messages
+      redirect_to edit_note_path(@note)
     end
   end
 
