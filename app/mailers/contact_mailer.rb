9cc116a3ffba21bcc957bf24513879e5e34422c3
taka.kita <80875058+usgreenfox@users.noreply.github.com>
2021-12-21 18:14:55 +0900

    Update contact_mailer.rb

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,6 @@
 class ContactMailer < ApplicationMailer
   def received_email(contact)
     @contact = contact
-    mail(to: ENV['USER_NAME'], subject: 'お問い合わせを承りました')
+    mail(to: ENV['USER_NAME'], subject: 'お問い合わせがありました')
   end
 end
