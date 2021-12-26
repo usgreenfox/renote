80f9b80d6f279ea095a78d939591993d67f8ba84
usgreenfox <usgreenfox@gmail.com>
2021-12-26 07:42:09 +0000

    Finish omni-auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,58 +12,10 @@ class User < ApplicationRecord
   has_many :reminds, dependent: :destroy
 
   validates :name, presence: true
-  
-  def self.without_sns_data(auth)
-    user = User.where(email: auth.info.email).first
-    
-    if user.present?
-      sns = SnsCredential.create(
-        uid: auth.uid,
-        provider: auth.provider,
-        user_id: user.id
-      )
-    else
-      user = User.new(
-        name: auth.info.name,
-        email: auth.info.email
-      )
-      sns = SnsCredential.new(
-        uid: auth.uid,
-        provider: auth.provider
-      )
-    end
-    return { user: user, sns: sns }
-  end
-  
-  def self.with_sns_data(auth, credential)
-    user = User.where(id: snscredential.user_id).first
-    unless user.present?
-      user = User.new(
-        name: auth.info.name,
-        email: auth.info.email
-      )
-    end
-    return { user: user }
-  end
-  
-  def self.find_oauth(auth)
-    uid = auth.uid
-    provider = auth.provider
-    snscredential = SnsCredential.where(uid: uid, provider: provider)
-    if snscredential.present?
-      user = with_sns_data(auth, snscredential)[:user]
-      sns = snscredential
-    else
-      user = without_sns_data(auth)[:user]
-      sns = without_sns_data(auth)[:sns]
-    end
-    return { user: user, sns: sns }
-  end
-  
+
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      # deviseのuserカラムに name を追加している場合は以下のコメントアウトも追記します
-      # user.name = auth.info.name
+      user.name = auth.info.name
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
     end
