475c0c62ddd7b55bec623e96efd377872274aa35
usgreenfox <usgreenfox@gmail.com>
2022-01-06 02:57:15 +0000

    Fix recommend#index

--- app/controllers/recommends_controller.rb
+++ app/controllers/recommends_controller.rb
@@ -1,6 +1,23 @@
 class RecommendsController < ApplicationController
   def index
-    user_entity = current_user.entities.order(salience: :DESC).first
-    @notes = Note.eager_load(:entities).where('entities.name LIKE?', "#{user_entity.name}").includes([:user])
+    # 文字数の少なすぎるノートは、エンティティが高くなる傾向のため除外
+    query = Entity.eager_load(:note).where('length(notes.body) >= 50')
+
+    # ユーザーがノートを作成していない場合、ユーザーはエンティティを持たないので
+    # 全エンティティで検索する
+    entities = query.where(user_id: current_user.id).order(salience: :DESC).limit(3)
+    if !entities.present?
+      entities = query.order(salience: :DESC).limit(3)
+    end
+
+    # 対象エンティティをもつノートを取得し、ランダムに10件抽出する
+    # 更新を押すごとにおすすめ内容が変わる仕様のため
+    sql = 'entities.name LIKE? or entities.name LIKE? or entities.name LIKE?'
+    @notes = Note
+      .eager_load(:entities)
+      .where(sql, "%#{entities[0].name}%", "%#{entities[1].name}%", "%#{entities[2].name}%")
+      .includes([:user])
+      .take(10)
+      # .order("RANDOM()").limit(10) or take(10)か要検討
   end
 end
