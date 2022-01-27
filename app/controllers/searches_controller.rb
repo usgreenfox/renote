9368f5d179b3e124b8346a5896c28fac282f976c
usgreenfox <usgreenfox@gmail.com>
2022-01-27 08:54:55 +0000

    Add paginate to search,recommends,mypage

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -10,10 +10,10 @@ class SearchesController < ApplicationController
   def search_for(model, keyword)
     # タグのバッジをクリック時、完全一致
     if model == 'tag'
-      Tag.find_by("name =?", keyword).notes.includes([:user])
+      Tag.find_by("name =?", keyword).notes.includes([:user]).page(params[:page])
     # 検索窓に入力時、部分一致
     elsif model == 'note'
-      Note.eager_load(:tags).where("title LIKE? or body LIKE? or tags.name LIKE?", "%#{keyword}%", "%#{keyword}%", "%#{keyword}%").includes([:user])
+      Note.eager_load(:tags).where("title LIKE? or body LIKE? or tags.name LIKE?", "%#{keyword}%", "%#{keyword}%", "%#{keyword}%").includes([:user]).page(params[:page])
     end
   end
 end
